9e0cf3e50dbaabe0910eaab3651c970203ac6da6
akhomuto <andrey.khomutov@t-systems.com>
2020-05-18 01:25:05 +0300

    add getAllItems

--- src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -1,7 +1,22 @@
 package com.learnreactivespring.controller;
 
+import com.learnreactivespring.domain.Item;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ItemClientController {
+
+    WebClient webClient = WebClient.create("http://localhost:8080");
+
+    @GetMapping("/client/retrieve")
+    public Flux<Item> getAllItemsUsingRetrieve() {
+        return webClient.get().uri("/v1/items")
+                .retrieve()
+                .bodyToFlux(Item.class)
+                .log("Item in client project");
+    }
+
 }
