62f3b07742be0458935932a963aa636a534e8168
akhomuto <andrey.khomutov@t-systems.com>
2020-05-26 00:57:39 +0300

    add insertOneItem with exchange/retrieve

--- src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -1,7 +1,10 @@
 package com.learnreactivespring.controller;
 
 import com.learnreactivespring.domain.Item;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -49,6 +52,19 @@ public class ItemClientController {
                 .exchange()
                 .flatMap(clientResponse -> clientResponse.bodyToMono(Item.class))
                 .log("Item in client project retrieve single item");
+    }
+
+    @PostMapping("/client/createItem")
+    public Mono<Item> createItem(@RequestBody Item item) {
+
+        Mono<Item> itemMono = Mono.just(item);
+
+        return webClient.post().uri("/v1/items")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemMono, Item.class)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Item in client project insert single item");
     }
 
 }
