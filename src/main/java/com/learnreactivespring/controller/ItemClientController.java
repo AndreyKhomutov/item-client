b8fe4b2badac4c9ae648930d96d0a49da7b85896
akhomuto <andrey.khomutov@t-systems.com>
2020-05-26 01:01:51 +0300

    add updateOneItem with exchange/retrieve

--- src/main/java/com/learnreactivespring/controller/ItemClientController.java
+++ src/main/java/com/learnreactivespring/controller/ItemClientController.java
@@ -3,7 +3,9 @@ package com.learnreactivespring.controller;
 import com.learnreactivespring.domain.Item;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -65,6 +67,20 @@ public class ItemClientController {
                 .retrieve()
                 .bodyToMono(Item.class)
                 .log("Item in client project insert single item");
+    }
+
+    @PutMapping("/client/updateItem/{id}")
+    public Mono<Item> updateItem(@PathVariable String id,
+                                 @RequestBody Item item) {
+
+        Mono<Item> itemMono = Mono.just(item);
+
+        return webClient.put().uri("/v1/items/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(itemMono, Item.class)
+                .retrieve()
+                .bodyToMono(Item.class)
+                .log("Item in client project update single item");
     }
 
 }
